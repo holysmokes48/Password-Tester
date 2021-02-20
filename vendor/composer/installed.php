a2b7af6d4f67572eb3084407dfc1bbc6a92b9a85
Shirley Ni <snssatb@gmail.com>
2021-02-20 10:07:48 -0500

    composer and dependencies

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '6b52850860a5cae3ae81edd9af9e41cfe8ff287c',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6b52850860a5cae3ae81edd9af9e41cfe8ff287c',
+    ),
+  ),
+);
